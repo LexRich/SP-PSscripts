35c93185a683bda960c34e1c4efd3bcc2c64538d
Petr Osavelyuk <osavelyukpn@gmail.com>
2018-04-11 17:22:38 +0700

    Created file DelHomeDirs.ps1

---
+++ DelHomeDirs.ps1
@@ -0,0 +1,10 @@
+﻿
+$query = Get-ADUser -SearchBase 'OU=Testing,DC=net,DC=stroypark,DC=su' -Filter {Enabled -eq "False"}
+$users = $query.SamAccountName | Tee-Object -f D:\dirs.txt
+#$users = Get-ChildItem D:\Users
+foreach ($user in $users){
+    $userpath = "D:\Users\$user"
+    
+        Remove-Item "$userpath" -Force -Recurse -ErrorAction SilentlyContinue -WhatIf
+    
+}
